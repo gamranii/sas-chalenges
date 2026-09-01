faed74943fe1604caad5e202b74d89145dddbdb2
gamranii <gamraniwork@gmail.com>
2026-09-01 10:07:23 +0100

    second chalenge

--- sas_chalenges.js
+++ sas_chalenges.js
@@ -1,5 +1,16 @@
+//challenge 1
 let Pdeudo = "fortnite";
 let Niveau = 50;
 let Age = 22;
 let score = 65468;
 let paye = "Maroc";
+
+//chalenge 2
+let EURO = 11;
+let MAD = EURO / 11;
+
+console.log("Budget en MAD :", EURO*200, + " ", "MAD");
+console.log("Budget en EUR :", MAD*200, +" ", "EURO");
+
+let x = prompt("entrer le nombre dr dirham:");
+console.log = x*EURO;
