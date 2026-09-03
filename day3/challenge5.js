019079b597bad658ecec0ca60a3b19dd91c5690d
gamranii <gamraniwork@gmail.com>
2026-09-03 13:54:38 +0100

    first boucle while challenge

--- day3/challenge5.js
+++ day3/challenge5.js
@@ -2,7 +2,9 @@ let prompt = require('prompt-sync')()
 let N = +prompt ('entrer  un nombre N : ')
 let X = +prompt('entrer un nembre X : ')
 
+
 for ( i = X ; i <= N ; i+=X ){
+    
     console.log(i)
 
 }
---
+++ day3/challenge6.js
@@ -0,0 +1,7 @@
+let prompt = require('prompt-sync')()
+let num = prompt ('entrer un nombre :')
+let i=0
+while (i<num){
+    console.log(num)
+    num-=1
+}
