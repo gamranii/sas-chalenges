b28d8b7b66ca7a858875b73f6ec3d5ec4777bcb8
gamranii <gamraniwork@gmail.com>
2026-09-03 11:59:56 +0100

    challenge 5 boucle for

---
+++ day3/challenge5.js
@@ -0,0 +1,8 @@
+let prompt = require('prompt-sync')()
+let N = +prompt ('entrer  un nombre N : ')
+let X = +prompt('entrer un nembre X : ')
+
+for ( i = X ; i <= N ; i+=X ){
+    console.log(i)
+
+}
